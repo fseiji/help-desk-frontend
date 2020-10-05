53b6fad22d5f52d8536e9fb3ab82a95d4c238219
Fernando Seiji Muramatsu <Fernando.Seiji@riachuelo.com.br>
2020-10-05 17:48:58 -0300

    Projeto Help Desk FrontEnd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,62 @@
+import { TicketService } from './services/ticket.service';
+import { DialogService } from './dialog.service';
+import { AuthGuard } from './components/security/auth.guard';
+import { AuthInterceptor } from './components/security/auth.interceptor';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { UserService } from './services/user.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/security/login/login.component';
+import { routes } from './app.routes';
+import { SharedService } from './services/shared.service';
+import { FormsModule } from '@angular/forms';
+import { UserNewComponent } from './components/user-new/user-new.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { TicketNewComponent } from './components/ticket-new/ticket-new.component';
+import { TicketListComponent } from './components/ticket-list/ticket-list.component';
+import { TicketDetailComponent } from './components/ticket-detail/ticket-detail.component';
+import { SummaryComponent } from './components/summary/summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    UserNewComponent,
+    UserListComponent,
+    TicketNewComponent,
+    TicketListComponent,
+    TicketDetailComponent,
+    SummaryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    routes
   ],
-  providers: [],
+  providers: [
+    UserService,
+    SharedService,
+    // Proteger as rotas, por ex. com canActivated só será permitido acessar home se tiver logado
+    DialogService,
+    TicketService,
+    AuthGuard,
+    // Com isso toda requisição HttpClient (Services) passa pelo Interceptor
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
